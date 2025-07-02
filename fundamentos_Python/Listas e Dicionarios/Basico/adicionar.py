7c2767946e23bb194d5986928ec54c2f9f77cdfd
GuilhermeFazito <fazitoguilherme@gmail.com>
2025-07-01 21:47:05 -0300

    Adicionar um item na lista

---
+++ fundamentos_Python/Listas e Dicionarios/Basico/adicionar.py
@@ -0,0 +1,8 @@
+# Crie uma lista de frutas com pelo menos 3 elementos e adicione uma nova fruta ao final da lista.
+
+lista = ["banana", "abacaxi", "Melancia"]
+fruta = input("Digite o nome da fruta: ")
+
+lista.append(fruta)
+
+print(lista)
